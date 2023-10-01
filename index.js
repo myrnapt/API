2a4ab01f2ce6648da6490bbb925797d7eb97eaee
Myrna Prieto <myrnapt96@gmail.com>
2023-10-01 22:50:27 +0200

    db casi acabada

--- index.js
+++ index.js
@@ -1,53 +1,19 @@
-var Express = require("express");
-var MongoClient = require("mongodb").MongoClient;
-var cors = require("cors");
-const multer = require("multer");
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
 
-var app = Express();
-app.use(cors());
+// CREAR SERVIDOR
+const app = express();
 
-var CONNECTION_STRING =
-  "mongodb+srv://admin:3aNVj7bYnY2yqFb0@cluster0.cl3aycg.mongodb.net/?retryWrites=true&w=majority";
+// CONECTAMOS A LA BASE DE DATOS
+conectarDB();
 
-var DATABASENAME = "mercados-mediavales";
+app.use(cors())
 
-var database;
+app.use(express.json());
+
+app.use('/api/mercados-mediavales', require('./routes/eventos'))
 
 app.listen(5038, () => {
-  MongoClient.connect(CONNECTION_STRING, (error, client) => {
-    database = client.db(DATABASENAME);
-    console.log("Conectado");
-  });
-});
-
-app.get("/api/mercados-mediavales/get-mercados", (request, response) => {
-  database
-    .collection("mercados-mediavales-collection")
-    .find({})
-    .toArray((error, result) => {
-      response.send(result);
-    });
-});
-
-app.post(
-  "/api/mercados-mediavales/add-mercados",
-  multer().none(),
-  (request, response) => {
-    database
-      .collection("mercados-mediavales-collection")
-      .count({}, function (error, numOfDocs) {
-        database.collection("mercados-mediavales-collection").insertOne({
-          name: (numOfDocs + 1).toString(),
-          name: request.body.name,
-        });
-        response.json("Add corrrecto");
-      });
-  }
-);
-
-app.delete("/api/mercados-mediavales/delete-mercado", (request, response) => {
-  database.collection("mercados-mediavales-collection").deleteOne({
-    name: request.query.name,
-  });
-  response.json("Delete correcto");
-});
+  console.log('Conectado');
+})
---
+++ models/eventos.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const EventoSchema = mongoose.Schema({
+        email: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        dataStart: {
+            type: Date,
+            required: true
+        },
+        dataEnd: {
+            type: Date,
+            required: true
+        },
+        lat: {
+            type: Number,
+            required: true
+        },
+        lng: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        organizer: {
+            type: String,
+            required: false
+        },
+        telephone: {
+            type: Number,
+            required: false
+        },
+        price: {
+            type: Number,
+            required: false
+        },
+        image: {
+            type: String,
+            required: false
+        },
+        creationDate: {
+            type: Date,
+            default: Date.now()
+        }
+
+})
+
+module.exports = mongoose.model('Evento', EventoSchema)
